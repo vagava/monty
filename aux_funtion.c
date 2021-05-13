d980d13009dd8e30d56d6372f646d6866fa7169e
vagava <vagava.123@gmail.com>
2021-05-13 13:31:00 -0500

    Fixed sorting between numbers: positive or negative, and leters

--- aux_funtion.c
+++ aux_funtion.c
@@ -37,14 +37,19 @@ int converter_to_number(char *token, unsigned int line_number)
 
 	if (token == NULL)
 		exit_error_function("usage: push integer", line_number);
-	if (*token == '-')
-	{
-		token++;
-		if (*token < '0' || *token > '9')
+	if (*token == '-' || *token < '0' || *token > '9') /*if not int or negative*/
+	{	if (*token == '-')/*if negative*/
+		{
+			token++;
+			if (*token < '0' || *token > '9')
+				exit_error_function("usage: push integer", line_number);
+			else
+				token--;
+		}
+		else /*if is a leter*/
 			exit_error_function("usage: push integer", line_number);
-		else
-			token--;
 	}
+
 	n = atoi(token);
 	return (n);
 }
