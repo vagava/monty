95420f57395ca132a564bbbb6d0820d203d6c14c
vagava <vagava.123@gmail.com>
2021-05-13 22:57:38 -0500

    add f_sub function

--- handling_functions_2.c
+++ handling_functions_2.c
@@ -33,7 +33,6 @@ void f_swap(stack_t **header, unsigned int line_number)
  *@line_number: current line
  * Return: Nothing.
  */
-
 void f_add(stack_t **header, unsigned int line_number)
 {
 	stack_t *result = NULL;
@@ -44,6 +43,28 @@ void f_add(stack_t **header, unsigned int line_number)
 
 	result = (*header)->next;
 	new_n = (*header)->n + result->n;
+	result->n = new_n;
+	*header = result;
+	free(result->prev);
+	result->prev = NULL;
+
+}
+/**
+ * f_add - swaps the top two elements of the stack.
+ *@header: pointer to stack
+ *@line_number: current line
+ * Return: Nothing.
+ */
+void f_sub(stack_t **header, unsigned int line_number)
+{
+	stack_t *result = NULL;
+	int new_n = 0;
+
+	if (!*header || !(*header)->next)
+		exit_error_function("can't sub, stack too short", line_number);
+
+	result = (*header)->next;
+	new_n = result->n - (*header)->n;
 	result->n = new_n;
 	*header = result;
 	free(result->prev);
--- monty.h
+++ monty.h
@@ -61,4 +61,5 @@ void f_pint(stack_t **header, unsigned int line_number);
 void f_pop(stack_t **header, unsigned int line_number);
 void f_swap(stack_t **header, unsigned int line_number);
 void f_add(stack_t **header, unsigned int line_number);
+void f_sub(stack_t **header, unsigned int line_number);
 #endif /* MONTY_H */
